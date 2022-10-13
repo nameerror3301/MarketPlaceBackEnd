448b7ba87878b5e5033f1e674fb4bd18b08abfc8
root <nameerrror3301@gmail.com>
2022-10-13 18:16:35 +0200

    Create mini server

--- CodeBack/internal/app/run.go
+++ CodeBack/internal/app/run.go
@@ -1,1 +1,21 @@
 package app
+
+import (
+	"fmt"
+
+	fiber "github.com/gofiber/fiber/v2"
+)
+
+func Run() error {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	// fmt.Errorf("Err up server - %s", err)
+	if err := app.Listen(":8080"); err != nil {
+		return fmt.Errorf("Err up server - %s", err)
+	}
+	return nil
+}
